0e15df47258a5abcce6c897c95f55e9b510e8b17
m1nist3r <andriztan1@gmail.com>
2023-06-05 19:33:00 +0200

    init

---
+++ src/main/java/com/m1nist3r/order/OrderServiceTaskApplication.java
@@ -0,0 +1,12 @@
+package com.m1nist3r.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceTaskApplication.class, args);
+	}
+}
